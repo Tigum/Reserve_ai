6fa6609ddd0a64e1effc171ae2487122917d6be2
Thiago Scolari <thiagoscolari@MacBook-Pro-de-Thiago.local>
2018-11-28 13:49:44 +0100

    commit

---
+++ s3.js
@@ -0,0 +1,5 @@
+export const accessKey = "AKIAIUHHF3SCXDZ2UR4A"
+export const secretKey = "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"
+export const bucket = "tigum"
+export const region = "us-east-1"
+export const successActionStatus = 201
