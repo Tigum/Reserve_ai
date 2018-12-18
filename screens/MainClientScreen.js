34f55355689232390ec7a7631a236283e937bf09
Thiago Scolari <thiagoscolari@MacBook-Pro-de-Thiago.local>
2018-12-18 16:06:41 +0100

    commit

--- screens/MainClientScreen.js
+++ screens/MainClientScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, ScrollView } from 'react-native';
+import firebase from 'firebase';
 import { connect } from 'react-redux';
 import SearchBarHeader from '../components/SearchBarHeader'
 import { loadAvailableBusinesses } from '../actions'
@@ -8,13 +9,25 @@ import { Spinner } from '../components/Spinner'
 
 
 class MainClientScreen extends Component {
+    state = {
+        isMounted: false
+    }
 
     componentDidMount() {
-        this.props.loadAvailableBusinesses()
+        this.setState({ isMounted: true }, async () => {
+            const { currentUser } = await firebase.auth()
+            if (currentUser) {
+                this.props.loadAvailableBusinesses()
+            }
+        })
+    }
+
+    componentWillUnmount() {
+        this.setState({ isMounted: false })
     }
 
     render() {
-        if (this.props.loading) {
+        if (this.props.loading || !this.state.isMounted) {
             return <Spinner fontSize={11} text='PROCURANDO SERVIÇOS DISPONÍVEIS...' />
         }
 
